2c1e0b55e33cb76c2a7491fb315b69c8d14d1bfc
Kate <nemochka99@gmail.com>
2019-01-19 23:30:31 +0200

    Fixed bugs, improved comments

--- ScheduleConverter.py
+++ ScheduleConverter.py
@@ -98,9 +98,7 @@ def process_data(data):
     scd = Schedule(schedule_info)
     # processing info about classes and passing it to Schedule
     scd = get_classes(scd, divided_schedule[1:])
-    # returning list of classes + info about schedule
-    """exp = scd.create_spreadsheet()
-    return exp, schedule_info"""
+
     return scd
 
 
@@ -123,7 +121,7 @@ def create_spreadsheet(schedule):
     sheet.insert_cols(0, 2)
     curr_date = schedule.start
     shift = datetime.timedelta(days=7)
-    i = 4 # row number
+    i = 2 # row number
     while sheet['C' + str(i)].value is not None:
         for j in range(0, 14, 2):
             sheet['A' + str(i + j)] = curr_date
@@ -146,12 +144,27 @@ def create_spreadsheet(schedule):
     sheet.merge_cells('A2:G2')
     sheet.merge_cells('A3:B3')
 
+    # DELETING EMPTY ROWS
+    i = 4
+    while sheet['A' + str(i)].value is not None:
+        if (sheet['C' + str(i)].value is ''
+                and sheet['D' + str(i)].value is ''
+                and sheet['E' + str(i)].value is ''
+                and sheet['F' + str(i)].value is ''
+                and sheet['G' + str(i)].value is ''):
+            sheet.delete_rows(i, 2)
+        else:
+            i += 2
+
     # APPLYING STYLES
     s_wb = apply_styles(s_wb)
 
     # SAVING SPREADSHEET
     print('File {} is successfully saved'.format(filename))
     s_wb.save(filename)
+    del s_wb
+    del schedule
+    print(schedule)
 
 
 def apply_styles(workbook):
@@ -192,17 +205,16 @@ def apply_styles(workbook):
 
         return s_title, s_sub, s_day, s_date, s_disc, s_room, s_num, col_styles
 
-    def style_week(sheet, start, i, disc, room, num, date, fill):
-        """Applying styles to lessons and date cells"""
+    def style_week(sheet, start, i, disc, room, num, date, fill1, fill2, f):
         sheet.merge_cells('A{}:A{}'.format(start, i + 1))
         for j in range(start, i + 2, 2):
-            # lessons cells
+            # classes cells
             for k in sheet[j]:
                 k.style = disc
-                k.fill = fill
+                k.fill = fill1 if f % 2 == 0 else fill2
             for k in sheet[j + 1]:
                 k.style = room
-                k.fill = fill
+                k.fill = fill1 if f % 2 == 0 else fill2
 
             # date cells styling
             sheet['A' + str(j)].style = date
@@ -232,7 +244,9 @@ def apply_styles(workbook):
     fill1 = PatternFill(fgColor=col1, fill_type='solid')
     fill2 = PatternFill(fgColor=col2, fill_type='solid')
 
-    # applying styles to head section
+    # APPLYING STYLES
+
+    # head section
     for i in sheet[1]:
         i.style = title
     for i in sheet[2]:
@@ -240,180 +254,20 @@ def apply_styles(workbook):
     for i in sheet[3]:
         i.style = day
 
-    # applying styles to lessons section
-    i = 4 # row number
-    start = 4 # start of new week row
-    style = 0 # flag to choose style
-    while sheet['B' + str(i)].value is not None:
-        fill = fill1 if style % 2 == 0 else fill2
-        sheet = style_week(sheet, start, i, disc, room, num, date, fill)
-        if sheet['B' + str(i + 2)].value is not None and sheet['B' + str(i + 2)].value < sheet['B' + str(i)].value:
-            start = i + 2
-            style += 1
-        i += 2
-    return workbook
-
-
-"""
-def export_to_excel(ex_data):
-    #function takes ready data, creates xlsx file and passes all the information to it
-
-    def workbook_styles_init():
-        # COLORS
-        blk = Color('000000')
-        ttl = Color('992600')
-        col_stls = (Color('ffffcc'), Color('e6ffcc'))
-
-        # ALIGNMENT
-        al_center = Alignment(horizontal='center', vertical='center', wrap_text=True)
-        al_cent_rot = Alignment(horizontal='center', vertical='center', wrap_text=False, text_rotation=90)
-        al_bottom = Alignment(horizontal='center', vertical='bottom', wrap_text=True)
-        al_top = Alignment(horizontal='center', vertical='top', wrap_text=True)
-
-        # BORDERS
-        side = Side(color=blk, style='thin')
-        all_borders = Border(top=side, bottom=side, left=side, right=side)
-        without_bot = Border(top=side, left=side, right=side)
-        without_top = Border(bottom=side, left=side, right=side)
-
-        # FONTS
-        f_title = Font(name='Calibri Light', size=12, bold=True, color=ttl)
-        f_subtitle = Font(name='Calibri Light', size=10, color=ttl)
-        f_day_date = Font(name='Calibri Light', size=10)
-        f_disc = Font(name='Calibri', size=11)
-        f_room = Font(name='Calibri', size=9)
-
-        # STYLES
-        s_title = NamedStyle(name='title', font=f_title, alignment=al_bottom, border=without_bot)
-        s_sub = NamedStyle(name='subtitle', font=f_subtitle, alignment=al_top, border=without_top)
-        s_day = NamedStyle(name='day', font=f_day_date, alignment=al_center, border=all_borders)
-        s_date = NamedStyle(name='date', font=f_day_date, alignment=al_cent_rot, border=all_borders)
-        s_disc = NamedStyle(name='disc', font=f_disc, alignment=al_center, border=without_bot)
-        s_room = NamedStyle(name='room', font=f_room, alignment=al_top, border=without_top)
-        s_num = NamedStyle(name='snum', font=f_day_date, alignment=al_center, border=all_borders)
-
-        return s_title, s_sub, s_day, s_date, s_disc, s_room, s_num, col_stls
-
-    def style(sheet, start, i, disc, room, num, date, fill1, fill2, f):
-        sheet.merge_cells('A{}:A{}'.format(start, i + 1))
-        for j in range(start, i + 2, 2):
-            # classes cells
-            for k in sheet[j]:
-                k.style = disc
-                k.fill = fill1 if f % 2 == 0 else fill2
-            for k in sheet[j + 1]:
-                k.style = room
-                k.fill = fill1 if f % 2 == 0 else fill2
-
-            # date cells styling
-            sheet['A' + str(j)].style = date
-            sheet['A' + str(j)].number_format = 'DD.MM'
-            sheet['A' + str(j + 1)].style = date
-
-            # number cells styling
-            sheet.merge_cells('B{}:B{}'.format(j, j + 1))
-            sheet['B' + str(j)].style = num
-            sheet['B' + str(j + 1)].style = num
-
-        return sheet
-
-    # IMPORTING DATA
-    schedule_properties = ex_data[1]  # group, year, semester, start
-    classes = ex_data[0]
-
-    # CREATING WORKBOOK
-    print('Creating spreadsheet...')
-    filename = schedule_properties[0] + '.xlsx'
-    s_wb = openpyxl.workbook.Workbook()
-    sheet = s_wb.active
-    sheet.title = schedule_properties[0]
-
-    # ADDING INFORMATION
-    # classes
-    days = ['Понеділок', 'Вівторок', 'Середа', 'Четвер', 'П\'ятниця']
-    sheet.append(days)
-    for row in classes:
-        sheet.append(row)
-    # date + number
-    sheet.insert_cols(0, 2)
-    curr_date = schedule_properties[3]
-    shift = datetime.timedelta(days=7)
-    i = 2
-    while sheet['C' + str(i)].value is not None:
-        for j in range(0, 14, 2):
-            sheet['A' + str(i + j)] = curr_date
-            sheet['B' + str(i + j)] = (j + 2) // 2
-        curr_date += shift
-        i += 14
-    # header
-    sheet.insert_rows(0, 2)
-    sheet['A1'] = 'РОЗКЛАД НА {} СЕМЕСТР'.format(schedule_properties[2])
-    sheet['A2'] = 'Група {}, {}'.format(schedule_properties[0], schedule_properties[1])
-
-    # IMPORTING STYLES
-    title, subtitle, day, date, disc, room, num, col_st = workbook_styles_init()
-    s_wb.add_named_style(title)
-    s_wb.add_named_style(subtitle)
-    s_wb.add_named_style(day)
-    s_wb.add_named_style(date)
-    s_wb.add_named_style(disc)
-    s_wb.add_named_style(room)
-    s_wb.add_named_style(num)
-    col1, col2 = col_st
-    # fills
-    fill1 = PatternFill(fgColor=col1, fill_type='solid')
-    fill2 = PatternFill(fgColor=col2, fill_type='solid')
-
-    print(sheet)
-
-    # DELETING EMPTY ROWS, APPLYING STYLES AND MERGING CELLS
-    # dimensions
-    for i in range(2):
-        sheet.column_dimensions[chr(ord('A') + i)].width = 4
-    for i in range(5):
-        sheet.column_dimensions[chr(ord('C') + i)].width = 19
-
-    # title
-    sheet.merge_cells('A1:G1')
-    sheet.merge_cells('A2:G2')
-    sheet.merge_cells('A3:B3')
-    for i in sheet[1]:
-        i.style = title
-    for i in sheet[2]:
-        i.style = subtitle
-    for i in sheet[3]:
-        i.style = day
-
-    # classes
-    # deleting rows
-    i = 4
-    while sheet['A' + str(i)].value is not None:
-        if (sheet['C' + str(i)].value is ''
-                and sheet['D' + str(i)].value is ''
-                and sheet['E' + str(i)].value is ''
-                and sheet['F' + str(i)].value is ''
-                and sheet['G' + str(i)].value is ''):
-            sheet.delete_rows(i, 2)
-        else:
-            i += 2
-
-    # applying styles
+    # lessons section
     i = 4
     start = 4
     f = 0
     while sheet['B' + str(i)].value is not None:
         if sheet['B' + str(i + 2)].value is None:
-            sheet = style(sheet, start, i, disc, room, num, date, fill1, fill2, f)
+            sheet = style_week(sheet, start, i, disc, room, num, date, fill1, fill2, f)
         elif sheet['B' + str(i + 2)].value < sheet['B' + str(i)].value:
-            sheet = style(sheet, start, i, disc, room, num, date, fill1, fill2, f)
+            sheet = style_week(sheet, start, i, disc, room, num, date, fill1, fill2, f)
             start = i + 2
             f += 1
         i += 2
+    return workbook
 
-    print('File {} is successfully saved'.format(filename))
-    s_wb.save(filename)
-    del s_wb
-"""
 
 def main():
     while True:
